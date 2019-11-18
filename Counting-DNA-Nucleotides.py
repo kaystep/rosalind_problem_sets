21a7f348f35f132a2c5e9ad330542c19c1afc801
Stephen Kay <kaystep@umich.edu>
2019-11-18 14:27:18 -0500

    Add files via upload

---
+++ Counting-DNA-Nucleotides.py
@@ -0,0 +1,20 @@
+a_total = 0
+c_total = 0
+g_total = 0
+t_total = 0
+
+for s in string: 
+  if (s == 'A'):
+    a_total += 1
+  if (s == 'C'):
+    c_total += 1
+  if (s == 'G'):
+    g_total += 1
+  if (s == 'T'):
+    t_total += 1
+
+
+print(a_total)
+print(c_total)
+print(g_total)
+print(t_total)
